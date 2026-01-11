b48314c9a4c62bf5a14d3fd46bf955521ac3face
Robert Baumgartner <r.baumgartner-1@tudelft.nl>
2026-01-11 19:33:24 +0100

    Added footer, made menu fix to the top

---
+++ code/scrolling.js
@@ -0,0 +1,24 @@
+/**
+ * Note: I do not remember where and how I got this code. I might have copy-pasted 
+ * it from someone. If so, please let me know who deserves his/her rightful 
+ * credits here. Thank you.
+ */
+
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
